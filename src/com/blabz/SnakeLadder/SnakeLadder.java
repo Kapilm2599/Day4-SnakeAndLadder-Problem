6d75a8950d2f7ad36186e5db18128e3b27dbc32a
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-13 16:49:18 +0530

    Added : Player then checks for a option

--- src/com/blabz/SnakeLadder/SnakeLadder.java
+++ src/com/blabz/SnakeLadder/SnakeLadder.java
@@ -13,6 +13,30 @@ public class SnakeLadder {
 		Random random = new Random();
 		int dice = random.nextInt(6)+1;
 		System.out.println("dice roll result: "+dice);
+		
+		int userPosition = 0;  
+		int cases = random.nextInt(3);	
+		
+		switch (cases) { 
+		case 0:
+			System.out.println("No Play!!");   
+			break;    
+			
+		case 1:
+			System.out.println("Ladder!");   
+			userPosition = userPosition + dice; 
+			break;
+			
+		default:  
+			System.out.println("Snake!");
+			userPosition = userPosition - dice;
+
+			if (userPosition < 0) {
+				userPosition = 0;
+			}
+		}
+		System.out.println("Presnt Position of the User: " + userPosition);
+	
 	
 	}
 
