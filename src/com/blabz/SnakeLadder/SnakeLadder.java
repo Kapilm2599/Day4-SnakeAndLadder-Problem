8522372e640afbed50a29387152d2c430cdb224b
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-13 16:39:40 +0530

    Added : Player at start position zero

---
+++ src/com/blabz/SnakeLadder/SnakeLadder.java
@@ -0,0 +1,14 @@
+package com.blabz.SnakeLadder;
+
+public class SnakeLadder {
+	public static void main(String[] args) {
+		int start;
+	
+		start =  0; 
+	
+		System.out.println("Player Starting Position will be:~ ");
+		System.out.println(+start);
+	
+	}
+
+}
